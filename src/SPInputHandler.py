dbc4301e68cc0301b2652c382ba93fbd5a3a0038
TristanKeith8420 <tristankeith8420@gmail.com>
2022-03-10 19:07:06 -0600

    removing print statements

--- src/SPInputHandler.py
+++ src/SPInputHandler.py
@@ -15,12 +15,10 @@ valid_keys = [key.W, key.A, key.S, key.D]
 def on_key_press(symbol: key, modifier: key) -> None:
     # Check if its a key press that matters
     if symbol in valid_keys:
-        print("SPInputHandler: Adding {} pressed".format(symbol))  # Logging
         directions_pushed.append(symbol)
 
 
 def on_key_release(symbol: key, modifers: key) -> None:
     # If the keypress is in directions pushed remove it
     if symbol in directions_pushed:
-        print("SPInputHandler: Removing {} pressed".format(symbol))  # Logging
         directions_pushed.remove(symbol)
